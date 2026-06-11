5a0eda0e2842323c577bedd127f107910e81c2a5
Caroline <carolinegpayne@gmail.com>
2026-06-11 15:15:46 -0500

    Added time limits

--- tempSensor/collect_data.py
+++ tempSensor/collect_data.py
@@ -1,27 +1,31 @@
 import serial
 import time
+import numpy as np
 
 arduino_port = "/dev/ttyUSB0"
 baud_rate = 9600
 filename = "tempSensor.csv"
 duration = int(input("Collect data for how many seconds?: "))
-start = time.time()
 
 try:
     ser = serial.Serial(arduino_port, baud_rate, timeout=1)
     time.sleep(2)
+    start = time.time()
     print(f"Connected to Arduino on {arduino_port}")
 
     with open(filename, "w", encoding="utf-8") as file:
         print(f"Exporting data to '{filename}'... Press Ctrl+C to stop")
 
         while time.time() - start < duration:
+            time.sleep(1)
             if ser.in_waiting > 0:
                 raw_data = ser.readline()
                 decoded_line = raw_data.decode("utf-8").strip()
+                temperatures = np.loadtxt(filename)
 
                 if decoded_line:
-                    print(decoded_line)
+                    elapsed_seconds = int(time.time() - start)
+                    print(f"{elapsed_seconds} Seconds: {decoded_line}")
                     file.write(decoded_line + "\n")
                     file.flush()
 
